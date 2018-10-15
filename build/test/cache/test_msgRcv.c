edad72f1bd3e8fe7e89eb60e35cb4163e31d13cf
Marco Santarcangelo Zazzetta <marcosantar93@gmail.com>
2018-10-14 21:40:26 -0300

    test modificado para cada codigo

--- build/test/cache/test_msgRcv.c
+++ build/test/cache/test_msgRcv.c
@@ -21,14 +21,46 @@ void tearDown(void) {
 
 void test_MessageRcv(void) {
 
- uint8_t * msg = "0:0,PRESSURE,600,1";
+ uint8_t retVal;
 
- uint8_t retVal = msgProcess(msg);
+ uint8_t * msg00 = "00:0,PRESSURE,600,1";
+
+ retVal = msgProcess(msg00);
 
  UnityAssertEqualNumber((UNITY_INT)((0)), (UNITY_INT)((retVal)), (
 
 ((void *)0)
 
-), (UNITY_UINT)(15), UNITY_DISPLAY_STYLE_INT);
+), (UNITY_UINT)(16), UNITY_DISPLAY_STYLE_INT);
+
+ uint8_t * msg01 = "01:0";
+
+ retVal = msgProcess(msg01);
+
+ UnityAssertEqualNumber((UNITY_INT)((0)), (UNITY_INT)((retVal)), (
+
+((void *)0)
+
+), (UNITY_UINT)(19), UNITY_DISPLAY_STYLE_INT);
+
+ uint8_t * msg02 = "02:0";
+
+ retVal = msgProcess(msg02);
+
+ UnityAssertEqualNumber((UNITY_INT)((0)), (UNITY_INT)((retVal)), (
+
+((void *)0)
+
+), (UNITY_UINT)(22), UNITY_DISPLAY_STYLE_INT);
+
+ uint8_t * msgErr = "Invalid";
+
+ retVal = msgProcess(msgErr);
+
+ UnityAssertEqualNumber((UNITY_INT)((1)), (UNITY_INT)((retVal)), (
+
+((void *)0)
+
+), (UNITY_UINT)(25), UNITY_DISPLAY_STYLE_INT);
 
 }
--- test/test_msgRcv.c
+++ test/test_msgRcv.c
@@ -10,7 +10,17 @@ void tearDown(void) {
 }
 
 void test_MessageRcv(void) {
-	uint8_t * msg = "0:0,PRESSURE,600,1";
-	uint8_t retVal = msgProcess(msg);
+	uint8_t retVal;
+	uint8_t * msg00 = "00:0,PRESSURE,600,1";
+	retVal = msgProcess(msg00);
 	TEST_ASSERT_EQUAL(0, retVal);
+	uint8_t * msg01 = "01:0";
+	retVal = msgProcess(msg01);
+	TEST_ASSERT_EQUAL(0, retVal);
+	uint8_t * msg02 = "02:0";
+	retVal = msgProcess(msg02);
+	TEST_ASSERT_EQUAL(0, retVal);
+	uint8_t * msgErr = "Invalid";
+	retVal = msgProcess(msgErr);
+	TEST_ASSERT_EQUAL(1, retVal);
 }
